63570768ef7aade419a214f39e1495e78182103e
StepanKhymera <44585663+StepanKhymera@users.noreply.github.com>
2020-04-29 20:56:25 +0300

    added Lab 1 wed

---
+++ Software modeling and analisys/Interpreter/executer.cpp
@@ -0,0 +1,6 @@
+#include "executer.h"
+
+Executer::Executer()
+{
+
+}
---
+++ Software modeling and analisys/Interpreter/executer.h
@@ -0,0 +1,11 @@
+#ifndef EXECUTER_H
+#define EXECUTER_H
+
+
+class Executer
+{
+public:
+    Executer();
+};
+
+#endif // EXECUTER_H
